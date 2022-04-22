b93d877fb8a90c31fbbd15ebcc8cdd02ff49d5b3
Guzenko, Andriy <andriy.guzenko@ceridian.com>
2022-04-22 00:19:27 -0400

    push actual code

---
+++ index.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+$(document).ready(function () {
+    $('#submit').click(function (event) {
+        event.preventDefault();
+        var url = $('#domainUrl').val();
+        var headers = {};//{ 'X-Custom-Header': 'value' };
+        $.get({
+            url: url,
+            data: {},
+            headers: headers
+        }, function (response) {
+            console.log(response, url);
+            $('#requestResult').text(response + ' ' + url);
+        }).fail(function () {
+            $('#requestResult').text('error');
+        })
+
+        /*fetch(url, {})
+            .then(response => response.json())
+            .then(function (data) { console.log(data); });*/
+    });
+
+    $('#clear').click(function (event) {
+        event.preventDefault();
+        $('#requestResult').text("");
+    });
+});
+
+
